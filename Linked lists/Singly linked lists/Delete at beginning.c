710f5f3c3a265739833eec32fd5301d86c6f99c2
Sabarinathan <sabarinathanr.tech@gmail.com>
2026-03-14 21:57:52 +0530

    Create Delete at beginning.c

---
+++ Linked lists/Singly linked lists/Delete at beginning.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+struct node *head = NULL;
+
+void insert(int val){
+    struct node *newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = val;
+    newnode->next = head;
+    head = newnode;
+}
+
+void deleteBeg(){
+    struct node *temp = head;
+
+    if(head == NULL) return;
+
+    head = head->next;
+    free(temp);
+}
+
+void display(){
+    struct node *temp = head;
+    while(temp != NULL){
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    while(n--){
+        int x;
+        scanf("%d",&x);
+        insert(x);
+    }
+
+    deleteBeg();
+
+    display();
+
+    return 0;
+}
